9fe33fd454d699f99b3d88c8e7d089b707d32929
Brian Ail <zetapb@gmail.com>
2023-04-29 01:36:45 -0300

    Updated home route to have /casamiento

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import { createRouter, createWebHistory } from "vue-router"
 const routes = [
   {
     name: 'home',
-    path: '/',
+    path: '/casamiento',
     component: () => import('@/views/Home.vue')
   }
 ]
